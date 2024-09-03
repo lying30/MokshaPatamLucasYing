9979bb7386c02fd2c3223d2888dc09d9886fead6
lucas.ying@menloschool.org <lucas.ying@menloschool.org>
2024-09-02 21:27:23 -0700

    another work chunk of first attempt

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,10 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: [Lucas Ying]
  *
  */
 
@@ -19,6 +22,31 @@ public class MokshaPatam {
         // Implement some sort of Breadth First Search similar to Maze Solver in CS2
         // In order to do this I will probably need a way to access different points on the board
         // So maybe I start with implementing an array of points to access?
+
+
+        // I think this array is arraying the board
+        int[] board = new int[boardsize + 1];
+        for (int i = 1; i <= boardsize; i++){
+            board[i] = i;
+        }
+        for (int i = 0; i < ladders.length; i++){
+            board[ladders[i][0]] = ladders[i][1];
+        }
+        for (int i = 0; i < snakes.length; i++){
+            board[snakes[i][0]] = snakes[i][1];
+        }
+
+        // Go through and maybe use a queue like we did in maze solver,
+        // but this time we do not have to check for the right direction or if we hit a wall.
+        // All I am looking for is the fastest way to navigate through the board array while utilizing
+        // the ladders and avoiding the snakes
+
+        // Go through the next six spaces ahead of where the current spot is, and check if there is a ladder,
+        // if there is a ladder use that turn to go that numebr of spaces, and if there isn't go maximum distance
+        // without hitting a snake --> so 6 as long as there isnt a snake on 6
+        // If there are two ladders in the upcoming 6 spaces, compare the first number to see which one goes further up the board
+        //
+
 
 
         return 0;
